b3faea135ab540d152c00df444ffa915b309effa
rizki-ap <rizki.ap@gmail.com>
2026-04-07 23:48:25 +0700

    Update find_tdoa.m

--- matlab/find_tdoa.m
+++ matlab/find_tdoa.m
@@ -1,28 +1,29 @@
 % Specify the file name
-fil1 = 'ruger_1b_chan0_v0.wav'; % You can use your own file name
-fil2 = 'ruger_1b_chan1_v0.wav'; % You can use your own file name
-fil3 = 'ruger_1b_chan2_v0.wav'; % You can use your own file name
-fil4 = 'ruger_1b_chan3_v0.wav'; % You can use your own file name
-fil5 = 'ruger_1b_chan4_v0.wav'; % You can use your own file name
-fil6 = 'ruger_1b_chan5_v0.wav'; % You can use your own file name
-fil7 = 'glock3_chan6_v0.wav'; % You can use your own file name
-fil8 = 'glock3_mean_v0.wav'; % You can use your own file name
+file1 = 'ruger_1b_chan0_v0.wav'; % You can use your own file name
+file2 = 'ruger_1b_chan1_v0.wav'; % You can use your own file name
+file3 = 'ruger_1b_chan2_v0.wav'; % You can use your own file name
+file4 = 'ruger_1b_chan3_v0.wav'; % You can use your own file name
+file5 = 'ruger_1b_chan4_v0.wav'; % You can use your own file name
+file6 = 'ruger_1b_chan5_v0.wav'; % You can use your own file name
+file7 = 'glock3_chan6_v0.wav'; % You can use your own file name
+file8 = 'glock3_mean_v0.wav'; % You can use your own file name
 %D:\WORK\GunShotLocator\Dataset\edge-collected-gunshot-audio\glock_17_9mm_caliber
+
 % Read the audio file
-[y1, Fs] = audioread(fil1); 
-[y2, Fs] = audioread(fil6); 
+[y1, Fs] = audioread(file1); 
+[y2, Fs] = audioread(file6); 
 
 f_upsample = 4;
 % Upsample 4x
-y1 = interp(y1,f_upsample);
-y2 = interp(y2,f_upsample);
+%y1 = interp(y1,f_upsample);
+%y2 = interp(y2,f_upsample);
 
 % Play the audio using the sound function
 %sound(y, Fs); 
 
 % Optional: Plot the audio signal
 % Create a time vector
-info = audioinfo(fil1);
+info = audioinfo(file1);
 t = 0:1/Fs:f_upsample*info.Duration;
 t = t(round(end/3):round(2*end/3)-1); % Adjust the length to match y
 y1 = y1(round(end/3):round(2*end/3));
